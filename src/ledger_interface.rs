3a23e3f9e2e3255c484addf368a527f317146d40
Adrien Zinger <zinger.ad@gmail.com>
2022-02-08 12:08:28 +0100

    Update v0.4.1 of runtime and add some functions

--- src/ledger_interface.rs
+++ src/ledger_interface.rs
@@ -3,57 +3,59 @@ const LEDGER_PATH: &str = "./ledger.json";
 use std::sync::{Arc, Mutex};
 
 use anyhow::{bail, Result};
-use massa_sc_runtime::{Address, Bytecode};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Default, Deserialize, Serialize)]
 pub(crate) struct Entry {
-    pub(crate) database: std::collections::HashMap<String, Bytecode>,
-    pub(crate) bytecode: Option<Bytecode>,
+    pub(crate) database: std::collections::HashMap<String, Vec<u8>>,
+    pub(crate) bytecode: Option<Vec<u8>>,
     pub(crate) balance: u64,
 }
 
 impl Entry {
-    pub(crate) fn get_bytecode(&self) -> Result<Bytecode> {
+    pub(crate) fn get_bytecode(&self) -> Result<Vec<u8>> {
         match &self.bytecode {
             Some(bytecode) => Ok(bytecode.clone()),
             _ => bail!("Error bytecode not found"),
         }
     }
-    pub(crate) fn get_data(&self, key: &str) -> Result<Bytecode> {
+    pub(crate) fn get_data(&self, key: &str) -> Result<Vec<u8>> {
         match self.database.get(key) {
             Some(bytecode) => Ok(bytecode.clone()),
-            _ => Ok(Bytecode::default()),
+            _ => Ok(vec![]),
         }
+    }
+    pub(crate) fn has_data(&self, key: &str) -> bool {
+        self.database.contains_key(key)
     }
 }
 
 #[derive(Clone, Deserialize, Serialize, Default)]
-pub(crate) struct Ledger(std::collections::BTreeMap<Address, Entry>);
+pub(crate) struct Ledger(std::collections::BTreeMap<String, Entry>);
 
 impl Ledger {
-    pub(crate) fn get(&self, address: &Address) -> Result<Entry> {
+    pub(crate) fn get(&self, address: &str) -> Result<Entry> {
         match self.0.get(address) {
             Some(entry) => Ok(entry.clone()),
             _ => bail!("Entry {} not found", address),
         }
     }
-    pub(crate) fn set_module(&mut self, address: &Address, module: &Bytecode) {
+    pub(crate) fn set_module(&mut self, address: &str, module: &[u8]) {
         let mut entry = match self.get(address) {
             Ok(entry) => entry,
             _ => Entry::default(),
         };
-        entry.bytecode = Some(module.clone());
+        entry.bytecode = Some(module.to_vec());
         self.0.insert(address.to_owned(), entry);
     }
-    pub(crate) fn set_data_entry(&self, address: &Address, key: String, value: Bytecode) {
+    pub(crate) fn set_data_entry(&self, address: &str, key: String, value: Vec<u8>) {
         let mut entry = match self.get(address) {
             Ok(entry) => entry,
             _ => Entry::default(),
         };
         entry.database.insert(key, value);
     }
-    pub(crate) fn sub(&mut self, address: &Address, amount: u64) -> Result<()> {
+    pub(crate) fn sub(&mut self, address: &str, amount: u64) -> Result<()> {
         let entry = match self.get(address) {
             Ok(entry) => entry,
             _ => bail!("Cannot find address {} in the ledger", address),
@@ -65,10 +67,10 @@ impl Ledger {
                 amount
             )
         }
-        self.0.insert(address.clone(), entry);
+        self.0.insert(address.to_string(), entry);
         Ok(())
     }
-    pub(crate) fn add(&mut self, address: &Address, amount: u64) -> Result<()> {
+    pub(crate) fn add(&mut self, address: &str, amount: u64) -> Result<()> {
         let entry = match self.get(address) {
             Ok(entry) => entry,
             _ => bail!("Cannot find address {} in the ledger", address),
@@ -80,16 +82,33 @@ impl Ledger {
                 amount
             )
         }
-        self.0.insert(address.clone(), entry);
+        self.0.insert(address.to_string(), entry);
         Ok(())
+    }
+}
+
+/// The call item contains the address called and the amount sent by the caller
+#[derive(Clone, Default)]
+pub(crate) struct CallItem {
+    pub address: String,
+    pub coins: u64,
+}
+
+impl CallItem {
+    pub(crate) fn address(address: &str) -> Self {
+        Self {
+            address: address.to_string(),
+            coins: 0,
+        }
     }
 }
 
 #[derive(Clone, Default)]
 pub(crate) struct InterfaceImpl {
     ledger: Arc<Mutex<Ledger>>,
-    call_stack: Arc<Mutex<std::collections::VecDeque<Address>>>,
-    owned: Arc<Mutex<std::collections::VecDeque<Address>>>,
+    /// Stack of call items ordered
+    call_stack: Arc<Mutex<std::collections::VecDeque<CallItem>>>,
+    owned: Arc<Mutex<std::collections::VecDeque<String>>>,
 }
 
 impl InterfaceImpl {
@@ -101,7 +120,7 @@ impl InterfaceImpl {
         }
         Ok(ret)
     }
-    pub(crate) fn get_entry(&self, address: &Address) -> Result<Entry> {
+    pub(crate) fn get_entry(&self, address: &str) -> Result<Entry> {
         match self.ledger.lock() {
             Ok(ledger) => ledger.get(address),
             Err(err) => bail!("Interface get entry:\n{}", err),
@@ -114,10 +133,10 @@ impl InterfaceImpl {
             _ => Ok(()),
         }
     }
-    pub(crate) fn call_stack_push(&self, address: Address) -> Result<()> {
+    pub(crate) fn call_stack_push(&self, item: CallItem) -> Result<()> {
         match self.call_stack.lock() {
             Ok(mut cs) => {
-                cs.push_back(address);
+                cs.push_back(item);
                 Ok(())
             }
             Err(err) => bail!("Call sack err:\n{}", err),
@@ -134,21 +153,16 @@ impl InterfaceImpl {
             Err(err) => bail!("Call sack err:\n{}", err),
         }
     }
-    pub(crate) fn call_stack_peek(&self) -> Result<Address> {
+    pub(crate) fn call_stack_peek(&self) -> Result<CallItem> {
         match self.call_stack.lock() {
             Ok(cs) => match cs.back() {
-                Some(address) => Ok(address.clone()),
+                Some(item) => Ok(item.clone()),
                 None => bail!("Call stack err:\npeek failed"),
             },
             Err(err) => bail!("Call stack err:\n{}", err),
         }
     }
-    pub(crate) fn set_data_entry(
-        &self,
-        address: &Address,
-        key: &str,
-        value: Bytecode,
-    ) -> Result<()> {
+    pub(crate) fn set_data_entry(&self, address: &str, key: &str, value: Vec<u8>) -> Result<()> {
         match self.ledger.lock() {
             Ok(ledger) => {
                 ledger.set_data_entry(address, key.to_string(), value);
@@ -157,13 +171,13 @@ impl InterfaceImpl {
             Err(err) => bail!("Ledger data insertion err:\n{}", err),
         }
     }
-    pub(crate) fn get(&self, address: &Address) -> Result<Entry> {
+    pub(crate) fn get(&self, address: &str) -> Result<Entry> {
         match self.ledger.lock() {
             Ok(ledger) => ledger.get(address),
             Err(err) => bail!("Data get err:\n{}", err),
         }
     }
-    pub(crate) fn set_module(&self, address: &Address, module: &Bytecode) -> Result<()> {
+    pub(crate) fn set_module(&self, address: &str, module: &[u8]) -> Result<()> {
         match self.ledger.lock() {
             Ok(mut ledger) => {
                 ledger.set_module(address, module);
@@ -172,40 +186,40 @@ impl InterfaceImpl {
             Err(err) => bail!("Data get err:\n{}", err),
         }
     }
-    pub(crate) fn sub(&self, address: &Address, amount: u64) -> Result<()> {
+    pub(crate) fn sub(&self, address: &str, amount: u64) -> Result<()> {
         match self.ledger.lock() {
             Ok(mut ledger) => ledger.sub(address, amount),
             Err(err) => bail!("Balance sub err:\n{}", err),
         }
     }
-    pub(crate) fn add(&self, address: &Address, amount: u64) -> Result<()> {
+    pub(crate) fn add(&self, address: &str, amount: u64) -> Result<()> {
         match self.ledger.lock() {
             Ok(mut ledger) => Ok(ledger.add(address, amount)?),
             Err(err) => bail!("Balance add err:\n{}", err),
         }
     }
-    pub(crate) fn callstack_to_vec(&self) -> Result<Vec<Address>> {
+    pub(crate) fn callstack_to_vec(&self) -> Result<Vec<String>> {
         match self.call_stack.lock() {
-            Ok(cs) => Ok(cs.clone().into()),
+            Ok(cs) => Ok(cs.iter().map(|item| item.address.to_owned()).collect()),
             Err(err) => bail!("Call sack err:\n{}", err),
         }
     }
-    pub(crate) fn owned_to_vec(&self) -> Result<Vec<Address>> {
+    pub(crate) fn owned_to_vec(&self) -> Result<Vec<String>> {
         match self.owned.lock() {
             Ok(owned) => Ok(owned.clone().into()),
             Err(err) => bail!("Call sack err:\n{}", err),
         }
     }
-    pub(crate) fn own(&self, address: &Address) -> Result<bool> {
+    pub(crate) fn own(&self, address: &str) -> Result<bool> {
         match self.owned.lock() {
-            Ok(owned) => Ok(owned.contains(address)),
+            Ok(owned) => Ok(owned.contains(&address.to_owned())),
             Err(err) => bail!("Call sack err:\n{}", err),
         }
     }
-    pub(crate) fn own_insert(&self, address: &Address) -> Result<()> {
+    pub(crate) fn own_insert(&self, address: &str) -> Result<()> {
         match self.owned.lock() {
             Ok(mut owned) => {
-                owned.push_back(address.clone());
+                owned.push_back(address.to_string());
                 Ok(())
             }
             Err(err) => bail!("Call sack err:\n{}", err),
@@ -222,7 +236,7 @@ impl InterfaceImpl {
         match self.call_stack.lock() {
             Ok(mut call_stack) => {
                 call_stack.clear();
-                call_stack.push_back("sender".to_string());
+                call_stack.push_back(CallItem::address("sender"));
             }
             Err(err) => bail!("Call sack err:\n{}", err),
         };
