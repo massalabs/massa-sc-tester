743b82efa8b76f89c6e2d18d0ebeb3aa26bcda14
Thomas Plisson <thomas.plisson@epitech.eu>
2022-04-21 10:33:00 +0200

    Change argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -6,37 +6,48 @@ mod interface_impl;
 mod ledger_interface;
 mod types;
 
-fn read_files() -> Result<Vec<(String, Vec<u8>)>> {
+pub struct Arguments {
+    filename: String,
+    module: Vec<u8>,
+    function: Option<String>,
+    caller: Option<ledger_interface::CallItem>,
+}
+
+fn read_arguments() -> Result<Arguments> {
     let args: Vec<String> = env::args().collect();
-    let mut ret = vec![];
-    #[allow(clippy::needless_range_loop)]
-    for i in 1..args.len() {
-        let name = args[i].clone();
-        let path = Path::new(&name);
-        if !path.is_file() {
-            bail!("{} isn't file", name)
-        }
-        let extention = path.extension().unwrap_or_default();
-        if extention != "wasm" {
-            bail!("{} should be .wasm", name)
-        }
-        let bin = fs::read(path)?;
-        ret.push((path.to_str().unwrap().to_string(), bin));
+    let len = args.len();
+    println!("{}", len);
+    if !(2..=5).contains(&len) {
+        bail!("invalid number of arguments")
     }
-    Ok(ret)
+    // parse the file
+    let name = args[1].clone();
+    let path = Path::new(&name);
+    if !path.is_file() {
+        bail!("{} isn't file", name)
+    }
+    let extension = path.extension().unwrap_or_default();
+    if extension != "wasm" {
+        bail!("{} should be .wasm", name)
+    }
+    let bin = fs::read(path)?;
+    Ok(Arguments {
+        filename: path.to_str().unwrap().to_string(),
+        module: bin,
+        function: None,
+        caller: None,
+    })
 }
 
 fn main() -> Result<()> {
-    let modules = read_files()?;
+    let args: Arguments = read_arguments()?;
     let ledger_context = ledger_interface::InterfaceImpl::new()?;
-    for (filename, module) in modules.into_iter() {
-        ledger_context.reset_addresses()?;
-        println!("run {}", filename);
-        println!(
-            "remaining points: {}",
-            run_main(&module, 1_000_000_000_000, &ledger_context)?
-        );
-    }
+    ledger_context.reset_addresses()?;
+    println!("run {}", args.filename);
+    println!(
+        "remaining points: {}",
+        run_main(&args.module, 1_000_000_000_000, &ledger_context)?
+    );
     ledger_context.save()?;
     Ok(())
 }
