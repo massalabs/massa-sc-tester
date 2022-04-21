36067203fd959437140beccc3b9c41547f94f6f6
Thomas Plisson <thomas.plisson@epitech.eu>
2022-04-21 11:23:18 +0200

    Update.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::{bail, Result};
-use massa_sc_runtime::run_main;
+use massa_sc_runtime::{run_function, run_main};
 use std::{collections::HashMap, env, fs, path::Path};
 
 mod interface_impl;
@@ -11,7 +11,7 @@ use ledger_interface::{CallItem, InterfaceImpl};
 pub struct Arguments {
     filename: String,
     module: Vec<u8>,
-    function: Option<String>,
+    function: Option<(String, String)>,
     caller: Option<CallItem>,
 }
 
@@ -37,7 +37,7 @@ fn parse_arguments() -> Result<Arguments> {
     let bin = fs::read(path)?;
 
     // parse the configuration parameters
-    let p_list: [&str; 3] = ["function", "sender_address", "coins"];
+    let p_list: [&str; 4] = ["function", "param", "addr", "coins"];
     let mut p: HashMap<String, String> = HashMap::new();
     for v in args.iter().skip(2) {
         let s: Vec<&str> = v.split('=').collect();
@@ -52,9 +52,16 @@ fn parse_arguments() -> Result<Arguments> {
     Ok(Arguments {
         filename: path.to_str().unwrap().to_string(),
         module: bin,
-        function: p.get_key_value("function").map(|x| x.1.clone()),
+        function: match (
+            p.get_key_value("function").map(|x| x.1.clone()),
+            p.get_key_value("param").map(|x| x.1.clone()),
+        ) {
+            (Some(function), Some(param)) => Some((function, param)),
+            (Some(function), None) => Some((function, "".to_string())),
+            _ => None,
+        },
         caller: match (
-            p.get_key_value("sender_address").map(|x| x.1.clone()),
+            p.get_key_value("addr").map(|x| x.1.clone()),
             p.get_key_value("coins").map(|x| x.1.clone()),
         ) {
             (Some(address), Some(coins)) => Some(CallItem {
@@ -75,11 +82,23 @@ fn parse_arguments() -> Result<Arguments> {
 fn main() -> Result<()> {
     let args: Arguments = parse_arguments()?;
     let ledger_context = InterfaceImpl::new()?;
-    ledger_context.reset_addresses()?;
+    if let Some(caller) = args.caller {
+        ledger_context.call_stack_push(caller)?;
+    }
     println!("run {}", args.filename);
     println!(
         "remaining points: {}",
-        run_main(&args.module, 1_000_000_000_000, &ledger_context)?
+        if let Some((name, param)) = args.function {
+            run_function(
+                &args.module,
+                1_000_000_000_000,
+                &name,
+                &param,
+                &ledger_context,
+            )?
+        } else {
+            run_main(&args.module, 1_000_000_000_000, &ledger_context)?
+        }
     );
     ledger_context.save()?;
     Ok(())
