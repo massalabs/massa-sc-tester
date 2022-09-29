c0b14a352fbca3f274dbe8d3553ad910362a8904
Thomas Plisson <thomas.plisson@epitech.eu>
2022-09-29 16:58:01 +0200

    Start updating execution config handling (complete).

--- src/main.rs
+++ src/main.rs
@@ -8,79 +8,73 @@ use execution_context::{CallItem, ExecutionContext, Slot};
 use indexmap::IndexMap;
 use json::{object, JsonValue};
 use massa_sc_runtime::{run_function, run_main};
-use serde::Deserialize;
-use std::{fs, fs::File, path::Path};
+use std::{collections::BTreeMap, fs, fs::File, path::Path};
+use step::ConfigStep;
 use structopt::StructOpt;
 
-#[derive(Debug, Deserialize)]
-struct StepArguments {
-    /// Path to the smart contract
-    path: String,
-    /// Function of the smart contract to be tested, default is 'main'
-    function: Option<String>,
-    /// Parameter of the given function
-    parameter: Option<String>,
-    /// Caller address
-    address: Option<String>,
-    /// Gas for execution
-    gas: u64,
-    /// Raw coins sent by the caller, default is '0', 1 raw_coin = 1e-9 coin
-    coins: Option<u64>,
-    /// Execution slot
+fn execute_step(
+    exec_context: &mut ExecutionContext,
     slot: Slot,
-}
-
-fn execute_step(exec_context: &mut ExecutionContext, args: StepArguments) -> Result<JsonValue> {
+    config_step: ConfigStep,
+) -> Result<JsonValue> {
     // init trace
     let mut trace = JsonValue::new_array();
 
-    // init the context for this step
-    exec_context.reset_addresses()?;
-    exec_context.call_stack_push(CallItem {
-        address: args.address.unwrap_or("default_addr".to_string()),
-        coins: args.coins.unwrap_or_default(),
-    })?;
-    exec_context.execution_slot = args.slot;
-
-    // read the wasm file
-    let path = Path::new(&args.path);
-    if !path.is_file() {
-        bail!("{} isn't a file", args.path)
-    }
-    let extension = path.extension().unwrap_or_default();
-    if extension != "wasm" {
-        bail!("{} extension should be .wasm", args.path)
-    }
-    let module = fs::read(path)?;
-
-    // run the function
-    let (remaining_gas, function_name) = if let Some(function) = args.function {
-        (
-            run_function(
-                &module,
-                args.gas,
-                &function,
-                &args.parameter.unwrap_or_default(),
-                exec_context,
-            )?,
+    // match the config step
+    match config_step {
+        ConfigStep::ExecuteSC {
+            path,
             function,
-        )
-    } else {
-        (
-            run_main(&module, args.gas, exec_context)?,
-            "main".to_string(),
-        )
-    };
+            parameter,
+            gas,
+            callstack,
+        } => {
+            // init the context for this step
+            exec_context.reset_addresses()?;
+            for call_item in callstack {
+                exec_context.call_stack_push(call_item)?;
+            }
+            exec_context.execution_slot = slot;
 
-    // push the function trace
-    let json = object!(
-        execute_function: {
-            name: function_name,
-            remaining_gas: remaining_gas,
-            output: exec_context.take_execution_trace()?,
+            // read the wasm file
+            let sc_path = Path::new(&path);
+            if !sc_path.is_file() {
+                bail!("{} isn't a file", path)
+            }
+            let extension = sc_path.extension().unwrap_or_default();
+            if extension != "wasm" {
+                bail!("{} extension should be .wasm", path)
+            }
+            let module = fs::read(sc_path)?;
+
+            // run the function
+            let (remaining_gas, function_name) = if let Some(function) = function {
+                (
+                    run_function(
+                        &module,
+                        gas,
+                        &function,
+                        &parameter.unwrap_or_default(),
+                        exec_context,
+                    )?,
+                    function,
+                )
+            } else {
+                (run_main(&module, gas, exec_context)?, "main".to_string())
+            };
+
+            // push the function trace
+            let json = object!(
+                execute_function: {
+                    name: function_name,
+                    remaining_gas: remaining_gas,
+                    output: exec_context.take_execution_trace()?,
+                }
+            );
+            trace.push(json)?;
         }
-    );
-    trace.push(json)?;
+        _ => (),
+    }
 
     // run the asynchronous messages
     for AsyncMessage {
@@ -145,19 +139,30 @@ fn main(args: CommandArguments) -> Result<()> {
         bail!("{} extension should be .json", args.config_path)
     }
     let config_slice = fs::read(path)?;
-    let executions_steps: IndexMap<String, StepArguments> = serde_json::from_slice(&config_slice)?;
+    let executions_config: BTreeMap<Slot, IndexMap<String, ConfigStep>> =
+        serde_json::from_slice(&config_slice)?;
 
     // execute the steps
     let mut trace = JsonValue::new_array();
-    for (step_name, step) in executions_steps {
-        let step_trace = execute_step(&mut exec_context, step)?;
-        let json = object!(
-            execute_step: {
-                name: step_name,
-                output: step_trace
+    for (slot, execution_steps) in executions_config {
+        let mut slot_trace = JsonValue::new_array();
+        for (step_name, step) in execution_steps {
+            let step_trace = execute_step(&mut exec_context, slot, step)?;
+            let step_json = object!(
+                execute_step: {
+                    name: step_name,
+                    output: step_trace
+                }
+            );
+            slot_trace.push(step_json)?;
+        }
+        let slot_json = object!(
+            execute_slot: {
+                slot: serde_json::to_string_pretty(&slot)?,
+                output: slot_trace
             }
         );
-        trace.push(json)?;
+        trace.push(slot_json)?;
     }
 
     // print the trace
